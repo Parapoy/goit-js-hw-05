5b8ad579ddefdb3c10fbcba8caa82e159a9fc7a5
Parapoy <145159048+Parapoy@users.noreply.github.com>
2024-05-01 03:09:43 +0200

    upd

---
+++ js/task-4.js
@@ -0,0 +1,51 @@
+const getTotalBalanceByGender = (users, gender) => {
+  let totalBalance = 0;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].gender === gender) {
+      totalBalance += users[i].balance;
+    }
+  }
+  return totalBalance;
+};
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
